9328e5b82d81ffac3c05c0bfa04857679bf89b8f
ogob31 <ogob29@gmail.com>
2026-01-09 07:44:06 +0100

    Fix credentials ID typo in SeedJobDSL.groovy

--- SeedJobDSL.groovy
+++ SeedJobDSL.groovy
@@ -11,7 +11,7 @@ projects.each { project ->
     def repoUrl = project.repo
     def branch = project.branch
     def jenkinsfilePath = project.jenkinsfile
-    def credentialsId = 'github_creds'
+    def credentialsId = 'github_cred'
 
     // Create pipeline job
     pipelineJob(pipelineName) {
