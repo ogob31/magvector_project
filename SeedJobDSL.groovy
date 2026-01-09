1c8ea07ffe4ca0fa7d5f71672becce60626c859c
ogob31 <ogob29@gmail.com>
2026-01-09 07:49:24 +0100

    Enhance SeedJobDSL to create folders and nested pipelines
    Refactor pipeline creation to support folder structure and configuration.

--- SeedJobDSL.groovy
+++ SeedJobDSL.groovy
@@ -1,31 +1,44 @@
 import groovy.json.JsonSlurper
 
 def pipelinesListFileName = 'pipelines_list.json'
+def config = new JsonSlurper().parse(new File("${WORKSPACE}", pipelinesListFileName))
 
-// Read the pipelines list from JSON file
-def projects = new JsonSlurper().parseText(new File("${WORKSPACE}", pipelinesListFileName).text)
+def credentialsId = 'github_cred'
 
-// Loop through the projects and create pipelines
-projects.each { project ->
-    def pipelineName = project.name
-    def repoUrl = project.repo
-    def branch = project.branch
-    def jenkinsfilePath = project.jenkinsfile
-    def credentialsId = 'github_cred'
+// 1) Create folders listed in rbac-folders
+def folders = config["rbac-folders"] ?: []
+folders.each { f ->
+    folder(f) {
+        displayName(f)
+        description("Auto-created by Seed Job from ${pipelinesListFileName}")
+    }
+}
 
-    // Create pipeline job
-    pipelineJob(pipelineName) {
-        definition {
-            cpsScm {
-                scm {
-                    git {
-                        remote {
-                            url(repoUrl)
-                            credentials(credentialsId)
+// 2) Create pipeline jobs inside each folder
+folders.each { f ->
+    def pipelines = config[f] ?: []
+
+    pipelines.each { p ->
+        def pipelineName    = p.name
+        def repoUrl         = p.repo
+        def branchName      = p.branch ?: "main"
+        def jenkinsfilePath = p.jenkinsfile
+
+        def jobFullName = "${f}/${pipelineName}"
+
+        pipelineJob(jobFullName) {
+            definition {
+                cpsScm {
+                    scm {
+                        git {
+                            remote {
+                                url(repoUrl)
+                                credentials(credentialsId)
+                            }
+                            branches("*/${branchName}")
                         }
-                        branches(branch)
-                        scriptPath(jenkinsfilePath)
                     }
+                    scriptPath(jenkinsfilePath)
                 }
             }
         }
