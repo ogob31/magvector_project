b2b4094994a307a6c493a2b47156e21cfef532ee
ogob31 <ogob29@gmail.com>
2026-01-10 14:33:13 +0100

    Change DynamoDB table name for Terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "magvector"
     key            = "magvector/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "magvector"
+    dynamodb_table = "magvector-tf-locks"
     encrypt        = true
   }
 }
