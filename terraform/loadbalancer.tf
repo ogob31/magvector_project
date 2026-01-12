1d45bc03733284d5cb734bb2bdd3904c1fe4ff67
ogob31 <ogob29@gmail.com>
2026-01-12 11:32:42 +0100

    Refactor to use existing ALB and target group

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -1,39 +1,26 @@
-resource "aws_lb" "main" {
-  name               = "test-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.ecs.id]
-  subnets            = var.subnet_ids
+#############################################
+# USE EXISTING ALB + TARGET GROUP + LISTENER
+#############################################
 
-  enable_deletion_protection = false
+data "aws_lb" "main" {
+  name = "test-lb"
 }
 
-resource "aws_lb_target_group" "main" {
-  name        = "test-tg"
-  port        = var.container_port
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"
-
-  health_check {
-    path                = "/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    matcher             = "200-399"
-  }
+data "aws_lb_target_group" "main" {
+  name = "test-tg"
 }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.main.arn
+# If you already have an HTTP :80 listener on test-lb, read it like this:
+data "aws_lb_listener" "http" {
+  load_balancer_arn = data.aws_lb.main.arn
   port              = 80
-  protocol          = "HTTP"
+}
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
-  }
+# Optional outputs (handy)
+output "alb_dns_name" {
+  value = data.aws_lb.main.dns_name
+}
 
-  depends_on = [aws_lb_target_group.main]
+output "target_group_arn" {
+  value = data.aws_lb_target_group.main.arn
 }
