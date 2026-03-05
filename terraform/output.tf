a3cfde7b210989b2b8d109bdc9b176ba716793b2
EC2 Default User <ec2-user@ip-172-31-31-109.eu-central-1.compute.internal>
2026-03-05 16:53:59 +0000

    Fix terraform outputs (remove duplicates + valid output blocks)

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,10 +2,18 @@ output "lb_hostname" {
   description = "The DNS name of the load balancer"
   value       = aws_lb.main.dns_name
 }
-output "target_group_arn" {
-  vpc_id = data.aws_vpc.selected.id
-}
-output "security_group_id" {
- vpc_id = data.aws_vpc.selected.id
 
+output "target_group_arn" {
+  description = "The ARN of the ALB target group"
+  value       = aws_lb_target_group.main.arn
+}
+
+output "alb_security_group_id" {
+  description = "Security Group ID for the ALB"
+  value       = aws_security_group.alb.id
+}
+
+output "ecs_security_group_id" {
+  description = "Security Group ID for the ECS service"
+  value       = aws_security_group.ecs.id
 }
